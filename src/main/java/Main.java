9da3540fe4b3c843c77fe221826a161b98c03f33
정현준 <jeongdalma@gmail.com>
2022-10-19 21:37:01 +0900

    CascadeType.ALL 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,19 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Child child1 = new Child();
+            Child child2 = new Child();
 
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+//            각 객체마다 영속상태로 바꿔줘야 한다.
+//            parent만 하게되면 parent만 저장되게 된다.
+//            CascadeType.ALL을 하게 되면 모두 저장된다.
+            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2);
 
             tx.commit();
         } catch (Exception e) {
